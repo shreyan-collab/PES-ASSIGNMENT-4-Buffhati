3a03564ab580fcc2b26a40b4d9dd0d7bd76dc6c9
shreyan-collab <89858015+shreyan-collab@users.noreply.github.com>
2021-10-11 23:09:45 -0600

    Add files via upload

---
+++ Release/source/switch.d
@@ -0,0 +1,22 @@
+source/switch.o source/switch.d: ../source/switch.c \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h \
+ ../source/switch.h
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h:
+
+../source/switch.h:
---
+++ Release/source/touchslider.d
@@ -0,0 +1,61 @@
+source/touchslider.o source/touchslider.d: ../source/touchslider.c \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\source/touchslider.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/board.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/fsl_device_registers.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4_features.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_clock.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_gpio.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/peripherals.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/pin_mux.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\utilities/fsl_debug_console.h
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\source/touchslider.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/board.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/fsl_device_registers.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4_features.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_clock.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_gpio.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/peripherals.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/pin_mux.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\utilities/fsl_debug_console.h:
