1eb2fd7e1ea97d2202a54ac4f78283d4475f9293
shreyan-collab <89858015+shreyan-collab@users.noreply.github.com>
2021-10-11 23:08:06 -0600

    Add files via upload

---
+++ Debug/source/statemachine.d
@@ -0,0 +1,73 @@
+source/statemachine.o source/statemachine.d: ../source/statemachine.c \
+ ../source/statemachine.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h \
+ ../source/timer.h ../source/touchslider.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/board.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/fsl_device_registers.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4_features.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_clock.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_gpio.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/peripherals.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/pin_mux.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h \
+ C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\utilities/fsl_debug_console.h \
+ ../source/switch.h ../source/pwm.h ../source/log.h
+
+../source/statemachine.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cm0plus.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmInstr.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/cmsis_gcc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/core_cmFunc.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/system_MKL25Z4.h:
+
+../source/timer.h:
+
+../source/touchslider.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/board.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/fsl_device_registers.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\CMSIS/MKL25Z4_features.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_clock.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_common.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\drivers/fsl_gpio.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/peripherals.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/pin_mux.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\board/clock_config.h:
+
+C:\Users\prabh\Documents\MCUXpressoIDE_11.2.0_4120\workspace\Buffhati_PES_Assignment_4\utilities/fsl_debug_console.h:
+
+../source/switch.h:
+
+../source/pwm.h:
+
+../source/log.h:
